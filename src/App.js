0a956470a796b02469b97832a3013ed58159ade3
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-11-20 22:41:58 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,51 +1,28 @@
-import React from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
-import AuthProvider from "contexts/auth";
-import CommonProvider from "contexts/common";
-import ProductsProvider from "contexts/products";
-import CartProvider from "contexts/cart";
-import CheckoutProvider from "contexts/checkout";
-import RouteWrapper from "layouts/RouteWrapper";
-import AuthLayout from "layouts/AuthLayout";
-import CommonLayout from "layouts/CommonLayout";
-import AuthPage from "pages/auth";
-import HomePage from "pages/home";
-import CheckoutPage from "pages/checkout";
-import "assets/scss/style.scss";
 
-const App = () => {
+import './App.css';
+import Nav from './Components/Nav';
+import Shop from './pages/shop/Shop'
+import Cart from './pages/cart/Cart' 
+
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import ShopContext from './context/shop-context';
+
+function App() {
   return (
-    <AuthProvider>
-      <CommonProvider>
-        <ProductsProvider>
-          <CartProvider>
-            <CheckoutProvider>
-              <Router>
-                <Switch>
-                  <RouteWrapper
-                    path="/"
-                    exact
-                    component={HomePage}
-                    layout={CommonLayout}
-                  />
-                  <RouteWrapper
-                    path="/checkout"
-                    component={CheckoutPage}
-                    layout={CommonLayout}
-                  />
-                  <RouteWrapper
-                    path="/auth"
-                    component={AuthPage}
-                    layout={AuthLayout}
-                  />
-                </Switch>
-              </Router>
-            </CheckoutProvider>
-          </CartProvider>
-        </ProductsProvider>
-      </CommonProvider>
-    </AuthProvider>
+    <div className="App">
+      <ShopContext>
+      <Router>
+        <Nav/>
+        <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+        </Routes>
+      </Router>
+      </ShopContext>
+      
+      
+    </div>
   );
-};
+}
 
 export default App;
